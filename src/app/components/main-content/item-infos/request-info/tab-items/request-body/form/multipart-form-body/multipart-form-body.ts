2666d0a17bbaf595d4ca2884500c6ff74b2c69dd
petruho2019 <vladgolovko20062018@mail.ru>
2026-04-14 00:58:27 +0300

    Много чего, схожу с ума

---
+++ src/app/components/main-content/item-infos/request-info/tab-items/request-body/form/multipart-form-body/multipart-form-body.ts
@@ -0,0 +1,42 @@
+import { Component, computed, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { RequestModel, TableRow } from '../../../../../../../../../../shared/models/requests/request';
+import { PandoraTable } from "../../../../../../../reuseable/pandora-table/pandora-table";
+import { BODY_KIND } from '../../../../../../../../../../shared/models/constants';
+import { MultipartBody } from '../../../../../../../../../../shared/models/requests/http/bodies/body';
+
+@Component({
+  selector: 'multipart-form-body',
+  imports: [PandoraTable],
+  templateUrl: './multipart-form-body.html',
+  styleUrl: './multipart-form-body.css',
+})
+export class MultipartFormBody implements OnChanges{
+
+  @Output() multipartChanged = new EventEmitter<TableRow[]>();
+  @Input() req: RequestModel;
+
+  tableInitialData: TableRow[] = [];
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['req']) {
+      const body = this.req.body[BODY_KIND.MULTIPART_FORM] as MultipartBody | undefined;
+
+      this.tableInitialData = body
+        ? body.fields.map(f => ({
+            id: f.id,
+            isActive: f.isActive,
+            name: f.key,
+            value: f.type === 'text' ? f.value : '',
+            multipartInfo: {
+              fileValue: f.type === 'file' ? f.file : null,
+              contentType: f.contentType ?? ''
+            }
+          }))
+        : [];
+    }
+  }
+
+  handleMultipartChanged(tableRows: TableRow[]) {
+    this.multipartChanged.emit(tableRows);
+  }
+}
---
+++ src/app/components/main-content/item-infos/request-info/tab-items/request-body/form/url-form-encoded-body/url-form-encoded-body.ts
@@ -0,0 +1,34 @@
+import { Component, computed, EventEmitter, Input, Output, SimpleChanges } from '@angular/core';
+import { RequestModel, TableRow } from '../../../../../../../../../../shared/models/requests/request';
+import { BODY_KIND } from '../../../../../../../../../../shared/models/constants';
+import { FormUrlEncodedBody } from '../../../../../../../../../../shared/models/requests/http/bodies/body';
+import { PandoraTable } from "../../../../../../../reuseable/pandora-table/pandora-table";
+
+@Component({
+  selector: 'url-form-encoded-body',
+  imports: [PandoraTable],
+  templateUrl: './url-form-encoded-body.html',
+  styleUrl: './url-form-encoded-body.css',
+})
+export class UrlFormEncodedBody {
+
+  @Output() urlEncodedChanged = new EventEmitter<TableRow[]>();
+  @Input() req: RequestModel;
+
+  tableInitialData: TableRow[] = [];
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['req']) {
+      const body = this.req.body[BODY_KIND.FORM_URL_ENCODED] as FormUrlEncodedBody | undefined;
+
+      this.tableInitialData = body
+        ? body.fields
+        : [];
+    }
+  }
+
+  handleURLEncodedChanged(tableRows: TableRow[]) {
+    this.urlEncodedChanged.emit(tableRows);
+  }
+  
+}
